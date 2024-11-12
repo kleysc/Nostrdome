054dfecb4f24e4b23ca3972606e93d1b38d562a6
kleyberthsantos <kleysb57@gmail.com>
2024-11-12 12:26:02 -0300

    Add new realys url mostro official

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,8 @@
 export const relayUrls = [
     //'wss://relay.damus.io',
-    'wss://relay.nostr.net',
+    //'wss://relay.nostr.net',
+    //'wss://relay.nostr.info',
     //'wss://relay.nostr.info',
     //'wss://nostr-pub.wellorder.net',
+    'wss://relay.mostro.network',
   ];
